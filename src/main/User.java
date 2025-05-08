c2bb58d242f693de9c4279d5302e4e17378ea335
KeonH-2 <kh.kim@kw.ac.kr>
2025-05-08 15:39:44 +0900

    개인정보 필드를 포함한 User 클래스 작성

--- src/main/User.java
+++ src/main/User.java
@@ -1,5 +1,47 @@
 package main;
 
 public class User {
+	private String name;
+	private String phonenumber;
+    private String id;
+    private String password;
+    
+    public User(String name, String phonenumber, String id, String password) {
+    	this.name = name;
+    	this.phonenumber = phonenumber;
+    	this.id = id;
+    	this.password = password;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public String getPhonenumber() {
+    	return phonenumber;
+    }
+    
+    public String getId() {
+    	return id;
+    }
 
+    public String getPassword() {
+    	return password;
+    }
+    
+    void setName() {
+    	this.name = name;
+    }
+    
+    void setPhonenumber() {
+    	this.phonenumber = phonenumber;
+    }
+    
+    void setId() {
+    	this.id = id;
+    }
+
+    void setPassword() {
+    	this.password = password;
+    }
 }
